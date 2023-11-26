aee1e59ac82c53077b17aa23cf1e9f329ec8116e
Marcos José <marcosjoseDV@hotmail.com>
2023-11-26 15:50:00 -0300

    Subindo projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trabalhando_Com_GitHub',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
